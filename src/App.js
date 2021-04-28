d26d998723e33280f1f14c53c7059ebbe8000ca3
mattia richetto <mattia.richetto@gmail.com>
2021-04-27 18:56:20 -0700

    Board

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+import classnames from 'classnames';
 import './App.css';
+import { files, ranks } from './domain/constants';
+
+function where(position) {
+  return position[0] === 'b' && position[1] === 1;
+}
 
 function App() {
-  return <div className="App">TODO</div>;
+  return (
+    <div>
+      {[...ranks].reverse().map((rank, index) => {
+        return (
+          <ul
+            className={classnames({
+              'Rank--even': index % 2 === 0,
+              'Rank--odd': index % 2 === 1
+            })}
+            key={rank}
+          >
+            {files.map((file, index) => (
+              <li
+                className={classnames({
+                  Knight: where([file, rank]),
+                  Square: !where([file, rank])
+                })}
+                data-file={file}
+                data-rank={rank}
+                key={file}
+              />
+            ))}
+          </ul>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/domain/constants.js
@@ -0,0 +1,2 @@
+export const files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']; // columns
+export const ranks = [1, 2, 3, 4, 5, 6, 7, 8]; // rows
