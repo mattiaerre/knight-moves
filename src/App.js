a3dc326c73510d8bb391882a829519add1a2d06b
mattia richetto <mattia.richetto@gmail.com>
2021-04-28 23:03:26 -0700

    add footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import classnames from 'classnames';
+import { name, version } from '../package.json';
 import './App.css';
 import { files, ranks } from './domain/constants';
 import { visited } from './domain/problem';
@@ -40,6 +41,9 @@ function App() {
           </ul>
         );
       })}
+      <footer>
+        {name} v{version}
+      </footer>
     </div>
   );
 }
